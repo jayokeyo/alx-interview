0867ee30a499ca3408bd490b570eeb27ae18c17b
jayokeyo <jaykopiyo@gmail.com>
2023-11-30 22:48:49 +0000

    More fixes

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,62 +1,50 @@
 #!/usr/bin/python3
-'''0x09. Island Perimeter'''
+'''
+Create a function def island_perimeter(grid): that returns
+the perimeter of the island described in grid:
+
+grid is a list of list of integers:
+0 represents water
+1 represents land
+Each cell is square, with a side length of 1
+Cells are connected horizontally/vertically (not diagonally).
+grid is rectangular, with its width and height not exceeding 100
+The grid is completely surrounded by water
+There is only one island (or nothing).
+The island doesn’t have “lakes” (water inside that isn’t
+connected to the water surrounding the island).
+'''
 
 
 def island_perimeter(grid):
-    '''returns the perimeter of the island described in grid'''
-    counter = 0
-    grid_max = len(grid) - 1  # index of the last list in the grid
-    lst_max = len(grid[0]) - 1  # index of the last square in list
+    '''returns the perimeter of the island described in grid
+    '''
+    perimeter = 0
 
-    for lst_idx, lst in enumerate(grid):
-        for land_idx, land in enumerate(lst):
-            if land == 1:
-                # left and right
-                if land_idx == 0:
-                    # left side
-                    counter += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                # if top cell is water or i = 0 perimeter++
+                if (grid[i - 1][j] == 0 and i != 0):
+                    perimeter += 1
+                if (i == 0):
+                    perimeter += 1
+                # if left cell is water or j = 0 perimeter++
+                if (grid[i][j - 1] == 0 and j != 0):
+                    perimeter += 1
+                if (j == 0):
+                    perimeter += 1
+                # if right cell is water or j = len(grid[i]) - 1
+                try:
+                    if (grid[i][j + 1] == 0):
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                # if bottom cell is water or i = len(grid) - 1
+                try:
+                    if (grid[i + 1][j] == 0):
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
 
-                    # right side
-                    if lst[land_idx + 1] == 0:
-                        counter += 1
-                elif land_idx == lst_max:
-                    # left side
-                    if lst[land_idx - 1] == 0:
-                        counter += 1
-
-                    # right side
-                    counter += 1
-                else:
-                    # left side
-                    if lst[land_idx - 1] == 0:
-                        counter += 1
-
-                    # right side
-                    if lst[land_idx + 1] == 0:
-                        counter += 1
-
-                # top and down
-                if lst_idx == 0:
-                    # top side
-                    counter += 1
-
-                    # bottom side
-                    if grid[lst_idx + 1][land_idx] == 0:
-                        counter += 1
-                elif lst_idx == grid_max:
-                    # top side
-                    if grid[lst_idx - 1][land_idx] == 0:
-                        counter += 1
-
-                    # bottom side
-                    counter += 1
-                else:
-                    # top side
-                    if grid[lst_idx - 1][land_idx] == 0:
-                        counter += 1
-
-                    # bottom side
-                    if grid[lst_idx + 1][land_idx] == 0:
-                        counter += 1
-
-    return counter
+    return perimeter
